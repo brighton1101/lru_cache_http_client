8b40bb11aa788debed8d1059edfe7da12e3635f8
Brighton Balfrey <balfrey@usc.edu>
2020-11-04 15:48:37 -0800

    added docblock descriptor

--- lru_cache_http_client/__init__.py
+++ lru_cache_http_client/__init__.py
@@ -4,5 +4,15 @@ from lru_cache_http_client.http.lru_http_client import LruHttpClient
 
 
 def get_caching_client(capacity=128, ttl_seconds=None):
+    """
+    Returns a caching HTTP client for making GET requets. LruHttpClient by
+    default uses the `requests` package to issue HTTP requests, so any params
+    that work for `requests.get` also work for `LruHttpClient.get`
+    :param capacity    - by default, 128. most performant if number supplied
+                         is a power of 2
+    :param ttl_seconds - by default, None. if provided, items in the cache
+                         will have a ttl policy with this value
+    :return an instance of `LruHttpClient`
+    """
     hasher = Hasher() if ttl_seconds is None else TtlHasher(seconds=ttl_seconds)
     return LruHttpClient(capacity, hasher=hasher)
