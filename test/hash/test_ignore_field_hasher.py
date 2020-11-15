e368a6927741ee8a1963411c66b13f45f3641bd8
Brighton Balfrey <balfrey@usc.edu>
2020-11-14 21:11:35 -0800

    cleaned up tests for ignore field hasher

--- test/hash/test_ignore_field_hasher.py
+++ test/hash/test_ignore_field_hasher.py
@@ -1,9 +1,18 @@
-from lru_cache_http_client.hash.ignore_fields_hasher import IgnoreFieldsHasher
+from lru_cache_http_client.hash.ignore_fields_hasher import (
+    IgnoreFieldsHasher,
+    _IgnoredField,
+)
 
 import pytest
 
 
 def test_ignore_field_setup_positional_invalid():
+    """
+    Given: IgnoreFieldsHasher is told to ignore a
+           positional arg that is out of bounds when
+           setup is called
+    Assert: IndexError is raised
+    """
     ignored_fields = [0, 1]
     hasher = IgnoreFieldsHasher(ignored_fields)
     with pytest.raises(IndexError):
@@ -11,6 +20,12 @@ def test_ignore_field_setup_positional_invalid():
 
 
 def test_ignore_field_setup_positional():
+    """
+    Given: IgnoreFieldsHasher is passed an ignored field
+           in the setup method for a positional arg twice
+    Assert: The hash values for each call on the ignored
+            field are the same
+    """
     ignored_fields = [0, 1]
     hasher = IgnoreFieldsHasher(ignored_fields)
     args1, _ = hasher.setup("a", "b")
@@ -20,8 +35,42 @@ def test_ignore_field_setup_positional():
 
 
 def test_ignore_field_setup_keyword():
+    """
+    Given: IgnoreFieldsHasher is passed an ignored field
+           in the setup method for a keyword arg twice
+    Assert: The hash values for each call on the ignored
+            field are the same
+    """
     ignored_fields = ["hello"]
     hasher = IgnoreFieldsHasher(ignored_fields)
     _, kwargs1 = hasher.setup(hello="world")
     _, kwargs2 = hasher.setup(hello="world2")
     assert kwargs1["hello"].__hash__() == kwargs2["hello"].__hash__()
+
+
+def test_ignore_field_teardown_positional():
+    """
+    Given: IgnoreFieldsHasher is passed an ignored field
+           in the teardown method for a positional arg
+    Assert: The keyword arg's value is converted back to
+            original value
+    """
+    field = _IgnoredField(1)
+    ignored_fields = [0]
+    hasher = IgnoreFieldsHasher(ignored_fields)
+    args, _ = hasher.teardown(field)
+    assert args[0] == 1
+
+
+def test_ignore_field_teardown_keyword():
+    """
+    Given: IgnoreFieldsHasher is passed an ignored field
+           in the teardown method for a keyword arg
+    Assert: The keyword arg's value is converted back to
+            original value
+    """
+    field = _IgnoredField("hello world")
+    ignored_fields = ["hello"]
+    hasher = IgnoreFieldsHasher(ignored_fields)
+    _, kwargs = hasher.teardown(hello=field)
+    assert kwargs["hello"] == "hello world"
