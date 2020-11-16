65b981e6e0ed7238f66d9d0eeeaeb9bc595db9eb
Brighton Balfrey <balfrey@usc.edu>
2020-11-15 16:13:20 -0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='lru_cache_http_client',
-    version='0.1',
+    version='0.0.1',
     author='Brighton Balfrey',
     author_email='balfrey@usc.edu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         'requests>=2.0'
